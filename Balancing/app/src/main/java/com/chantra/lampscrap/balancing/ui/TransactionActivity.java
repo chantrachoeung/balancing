0046f3156e2a60765b7786e09a2f703750f4fb42
phearom <phearom.pov2011@gmail.com>
2016-07-10 10:47:24 +0700

    -created UI (Transaction Activity with action back).

--- Balancing/app/src/main/java/com/chantra/lampscrap/balancing/ui/TransactionActivity.java
+++ Balancing/app/src/main/java/com/chantra/lampscrap/balancing/ui/TransactionActivity.java
@@ -1,15 +1,87 @@
 package com.chantra.lampscrap.balancing.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
 
 import com.chantra.lampscrap.balancing.R;
+import com.chantra.lampscrap.balancing.databinding.ActivityTransactionBinding;
+
+import java.util.Calendar;
 
 public class TransactionActivity extends AppCompatActivity {
+
+    private ActivityTransactionBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_transaction);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_transaction);
+        setSupportActionBar(mBinding.toolBar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mBinding.tvDateTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePicker();
+            }
+        });
+
+        mBinding.tranClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSubmit();
+            }
+        });
+    }
+
+    public void onSubmit() {
+        Intent intent = new Intent();
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent();
+        setResult(RESULT_CANCELED, intent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_transaction, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            case R.id.action_today:
+                datePicker();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void datePicker() {
+        Calendar c = Calendar.getInstance();
+        DatePickerDialog dialog = new DatePickerDialog(TransactionActivity.this, R.style.DialogTheme, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            }
+        }, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
+        dialog.show();
     }
 }
